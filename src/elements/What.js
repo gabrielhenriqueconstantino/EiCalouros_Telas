f11698fd93cc53f7df1cafb6dee6a604c9d3df32
gabrielhenriqueconstantino <gabrielconstantinogh@gmail.com>
2025-05-24 22:17:20 -0300

    Algumas modificações na seção What

--- src/elements/What.js
+++ src/elements/What.js
@@ -5,7 +5,17 @@ import '../styles/What.css';
 const What = () => {
   return (
     <section className="what-section">
-      <h2>O que é?</h2>
+      <div className='main'>
+        <h2>O que é?</h2>
+        <p>O 'Ei, Calouro!' é um site intuitivo e interativo que centraliza informações essenciais 
+        <br />e facilita a adaptação dos calouros da Anhembi Morumbi, proporcionando uma experiência 
+        <br />universitária mais acessível, organizada e integrada.</p>
+      </div>
+      
+      <div className='main-img'>
+        <img src='./img/students_retouch.png'></img>
+      </div>
+      
     </section>
   );
 };
